f3d5078abf7037b04b2e9858358309868e8c410f
Alex Conder <alex.conder@nerdweb.com.br>
2022-06-15 09:21:49 -0300

    feat: creating get notes route

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -9,6 +9,7 @@ const router = express.Router();
 //Models
 const Note = require('../models/Note');
 
+//Criar notas
 router.post('/', authenticate, async (req, res) => {
     try {
         const { title, text, user } = req.body; 
@@ -34,9 +35,26 @@ router.post('/', authenticate, async (req, res) => {
     }    
 })
 
+
+//Carregar notas do usuário
+router.get("/", authenticate, async(req, res) => {
+    try {
+        const { id } = req.body.user;
+        const response = await Note.find({ userID: id });
+
+        if(!response){
+            throw new Error('Ainda não há nenhuma nota cadastrada.');
+        }
+        
+        res.status(200).json({ response })
+    } catch (error) {
+        res.status(400).json({ error: error.message})   
+    }
+})
+
+//Excluir nota
 router.delete("/:noteId", authenticate, async(req, res) => {
     try {
-        console.log()
         const { noteId } = req.params; 
 
         if(!noteId){
