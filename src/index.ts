4a6f8020b30f196bc927344f5ff5d3f1d7b3c3a4
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-09-15 12:29:37 -0300

    removing comments

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 
-import server from "./server";
+import server from "./server"
 
 import mongoose from "mongoose";
 
-//Configuração de variável de ambiente para Heroku
 const port = process.env.PORT || 3030;
 
 mongoose.connect(process.env.DATABASE_URL as string)
--- src/server.ts
+++ src/server.ts
@@ -4,27 +4,18 @@ import cors from "cors";
 dotenv.config();
 
 import userRoutes from "./routes/userRoutes/user.routes";
-import noteRoutes from "./routes/noteRoutes/note.routes"
+import noteRoutes from "./routes/noteRoutes/note.routes";
 
 const app = express();
 
 app.use(cors());    
-//Métodos para o req.body funcionar
+
 app.use(
     express.urlencoded({
         extended: true,
     })
 )
 app.use(express.json());
-
-/*
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "DELETE, POST, PUT, GET, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-    next();   
-});
-*/
 
 app.use('/user', userRoutes);
 app.use('/notes', noteRoutes);
